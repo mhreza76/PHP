e0ed5385259c57eaef24e94d47877e6b4e6972dc
mhreza76 <rezabaiust@gmail.com>
2020-12-06 14:08:53 +0600

    data types

--- 06_data_types.php
+++ 06_data_types.php
@@ -1,14 +1,66 @@
 
 <?php
     /*php data types
-    1. string
-    2. integer
+    1. bool
+    2. int
     3. float
-    4. boolean
-    5. object
-    6. array
-    7. null
+    4. string
+    5. array
+    6. object
+    7. callable
+    8. iterable
+    9. resource
+    10. NULL
     */
+
+    // Boolean can be true or false 
+    $x = true;
+    $is_friend = false;
+    echo var_dump($x);
+    echo "<br>";
+    echo var_dump($is_friend);
+    echo "<br>";
+
+    //  Integer - non decimal number 
+    $income = 25000;
+    $name = "Reza";
+    echo "income = $income <br>";
+
+    // Float - Decimal point number
+    $average_number = 78.5;
+    echo $average_number;
+    echo "<br>";
+
+    // String - sequence of characters
+    $name = "Reza";
+    echo "My name is $name";
+    echo "<br>";
+
+    // Array - used to store multiple values in a single variable
+    $friends = array("daud", "hasib", "ome");
+    echo var_dump($friends);
+    echo "<br>";
+    echo $friends[0];
+    echo "<br>";
+
+    // Object - Instances of classes
+    class foo
+    {
+        function do_foo()
+        {
+            echo "Doing foo."; 
+        }
+    }
+
+    $bar = new foo;
+    $bar->do_foo();
+
+    // NULL - The special NULL value represents a variable with no value. 
+    $var = NULL;
+
+
+
+
 ?>
 
 

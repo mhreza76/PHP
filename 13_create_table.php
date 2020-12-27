e70c32235f90f4642411c71bae56b4aadfd4adb9
mhreza76 <rezabaiust@gmail.com>
2020-12-27 23:39:34 +0000

    table creation

--- 13_create_table.php
+++ 13_create_table.php
@@ -4,10 +4,10 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
-// $database = "baiust";
+$database = "baiust";
 
 // Create a connection to the DataBase
-$conn = mysqli_connect($serverName,$userName,$password);
+$conn = mysqli_connect($serverName,$userName,$password, $database);
 
 
 // Die if connection was not successful 
@@ -17,7 +17,7 @@ if(!$conn){
 echo "Connection was successful";
 
 // Create a table in DB 
-$sql = "CREATE TABLE `baiust`.`student_info` (`s_no` INT(6) NOT NULL AUTO_INCREMENT, `s_id` INT(7) NOT NULL, `name` CHAR(50) NOT NULL, `department` CHAR(50) NOT NULL", PRIMARY KEY (`s_id`))";
+$sql = "CREATE TABLE `student_info` (`s_no` INT(6) NOT NULL AUTO_INCREMENT, `s_id` INT(7) NOT NULL, `name` CHAR(50) NOT NULL, `department` CHAR(50) NOT NULL", PRIMARY KEY (`s_id`))";
 
 $result = mysqli_query($conn, $sql);
 

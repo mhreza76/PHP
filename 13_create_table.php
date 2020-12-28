77a8f4e488136c8eca11c7c45f9831af60890fec
mhreza76 <rezabaiust@gmail.com>
2020-12-28 00:47:56 +0000

    table creation

--- 13_create_table.php
+++ 13_create_table.php
@@ -4,10 +4,9 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
-$database = "baiust";
 
 // Create a connection 
-$conn = mysqli_connect($serverName,$userName,$password, $database);
+$conn = mysqli_connect($serverName,$userName,$password);
 
 
 // Die if connection was not successful 
@@ -17,19 +16,9 @@ if(!$conn){
 echo "Connection was successful <br>";
 
 // Create a table in DB 
-$sql = "CREATE TABLE student_info (s_no INT NOT NULL AUTO_INCREMENT,
-s_id INT(7) NOT NULL, name CHAR(50) NOT NULL,
-department CHAR(50) NOT NULL,
-PRIMARY KEY (`s_no`)
-)";
+$sql = "CREATE TABLE `student_info` (`s_no` INT(6) NOT NULL AUTO_INCREMENT, `s_id` INT(7) NOT NULL, `name` CHAR(50) NOT NULL, `department` CHAR(50) NOT NULL", PRIMARY KEY (`s_id`))";
 
 $result = mysqli_query($conn, $sql);
-// check database creation 
-if($result){
-    echo "The table is created successfully. <br>";
-}
-else{
-    echo "THe table not created, because of this error: ". mysqli_error($conn);
-}
+
 
 ?>
